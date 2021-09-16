839bf32adf7e2a20423273cc37c0ab5b54c4857c
Umakant-gosavi <83106105+Umakant-gosavi@users.noreply.github.com>
2021-09-16 21:48:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://innshare.herokuapp.com";
+const baseURL = "https://secret-anchorage-61876.herokuapp.com";
 //const baseURL = "http://localhost:3000"
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
